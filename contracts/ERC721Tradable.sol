f863970de5d824e38a55883bab8cf3bdc8a84b9e
QuantumlyTangled <ethoarx@gmail.com>
2021-04-03 13:56:03 +0200

    refactor: use counters for token ids

--- contracts/ERC721Tradable.sol
+++ contracts/ERC721Tradable.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.3;
 import "./utils/StringConcat.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -18,10 +19,10 @@ contract ERC721Tradable is ERC721, Ownable {
     using StringConcat for string;
     using Strings for string;
     using SafeMath for uint256;
-
+    using Counters for Counters.Counter;
 
     address proxyRegistryAddress;
-    uint256 private _currentTokenId = 0;
+    Counters.Counter public tokenIds;
 
     constructor(
         string memory _name,
@@ -36,24 +37,10 @@ contract ERC721Tradable is ERC721, Ownable {
      * @param _to address of the future owner of the token
      */
     function mintTo(address _to) public onlyOwner {
-        uint256 newTokenId = _getNextTokenId();
-        _mint(_to, newTokenId);
-        _incrementTokenId();
-    }
+        tokenIds.increment();
 
-    /**
-     * @dev calculates the next token ID based on value of _currentTokenId
-     * @return uint256 for the next token ID
-     */
-    function _getNextTokenId() private view returns (uint256) {
-        return _currentTokenId.add(1);
-    }
-
-    /**
-     * @dev increments the value of _currentTokenId
-     */
-    function _incrementTokenId() private {
-        _currentTokenId++;
+        uint256 newItemId = tokenIds.current();
+        _mint(_to, newItemId);
     }
 
     function baseTokenURI() public pure returns (string memory) {
