44346dc2c332f30245c27bbc9cce86ffa6627491
QuantumlyTangled <ethoarx@gmail.com>
2021-04-03 14:13:29 +0200

    feat: configure etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
 
-// TODO: Implement https://hardhat.org/plugins/nomiclabs-hardhat-etherscan.html
-
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API } = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -17,5 +16,8 @@ module.exports = {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API
   }
 };
