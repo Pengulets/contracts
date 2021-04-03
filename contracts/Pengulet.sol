4c7c89138dd24b47152569427a3ae5b407f765d9
QuantumlyTangled <ethoarx@gmail.com>
2021-04-03 09:06:58 +0200

    feat: basic pengulet contract design

--- contracts/Pengulet.sol
+++ contracts/Pengulet.sol
@@ -1,2 +1,14 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.3;
+
+import "./ERC721Tradable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Pengulet
+ */
+contract Pengulet is ERC721Tradable {
+    constructor(address _proxyRegistryAddress) ERC721Tradable("Pengulet", "PNGU", _proxyRegistryAddress) {}
+
+    // TODO: baseTokenURI AND contractURI
+}
