8df993e7526ee9bc45d1b439720057ad78a3bc53
QuantumlyTangled <ethoarx@gmail.com>
2021-04-04 17:22:29 +0200

    feat: create proxy deploy script

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -1,11 +1,14 @@
-const { ethers } = require('hardhat');
+import { ethers, upgrades } from 'hardhat';
 
 async function main() {
     const Pengulet = await ethers.getContractFactory('Pengulet');
 
     // Start deployment, returning a promise that resolves to a contract object
     // TODO: Change between Rinkeby and Mainet
-    const pengulet = await Pengulet.deploy('0xf57b2c51ded3a29e6891aba85459d600256cf317'); // OpenSea Rinkeby proxy adress
+    const proxyAddress = '0xf57b2c51ded3a29e6891aba85459d600256cf317'; // OpenSea Rinkeby proxy adress
+
+    const pengulet = await upgrades.deployProxy(Pengulet, [proxyAddress], { initializer: '__Pengulet_init' });
+    await pengulet.deployed();
     console.log('Contract deployed to address:', pengulet.address);
 }
 
